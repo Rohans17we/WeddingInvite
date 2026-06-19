712bd7dcb0fff768f2b1bbf0b59976d207a3b924
Rohans17we <rohans17we@gmail.com>
2026-06-19 20:05:33 +0530

    style: set /envelope background to white

--- app/envelope/page.tsx
+++ app/envelope/page.tsx
@@ -2,7 +2,7 @@
 
 export default function EnvelopePlayground() {
   return (
-    <main style={{ height: "100vh", background: "#1A0505", display: "flex", justifyContent: "center", alignItems: "center" }}>
+    <main style={{ height: "100vh", background: "#ffffff", display: "flex", justifyContent: "center", alignItems: "center" }}>
       {/* Blank Slate for Envelope Design */}
     </main>
   );
